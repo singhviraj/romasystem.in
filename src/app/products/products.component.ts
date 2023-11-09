43c34df1db610fcc9362ef7e9ba0ec3ad70ea8d3
unknown <test2mail2698@gmail.com>
2023-11-10 00:30:42 +0530

    some CSS changes mentioned by client

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -22,8 +22,7 @@ export class ProductsComponent {
     
   }
 
-  
-  showappnotification(eventd:any){
+   showappnotification(eventd:any){
     this.shownotification =true;
     console.log(this.selectedname);
    console.log(eventd.target.placeholder);
@@ -38,12 +37,12 @@ export class ProductsComponent {
   }
 products =[
   // {id:4 ,name:"activity tracer" ,price:'9999rs' ,color:"white",available:'Available', image:'assets/activity tracer.webp'}
-  {id:1 ,name:"Alexa" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Alexa.jpg',descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'2 years motor guarantee',descriptionfour:'Anti-dust'},
-  {id:2 ,name:"Bliss" ,colorone:"Smoke Brown",colortwo:"Plum wine", available:'Available', image:'assets/Bliss.jpg', descriptionone:'Copper winding',descriptiontwo:'GP blade technology',descriptionthree:'Energy saver',descriptionfour:'2 years motor guarantee'},
-  {id:3 ,name:"Imperial" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Imperial.jpg' , descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'2 years motor guarantee',descriptionfour:'Anti-dust'},
-  {id:4 ,name:"Sapphire" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Sapphire.jpg', descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'GP blade technology',descriptionfour:'2 years motor guarantee'},
-  {id:5 ,name:"Star" ,colorone:"Smoke Brown",colortwo:"Plum wine",available:'Available', image:'assets/Star.jpg' , descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'GP blade technology',descriptionfour:'2 years motor guarantee'},
-  {id:6 ,name:"Turbo+" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Turbo+.jpg' , descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'2 years motor guarantee',descriptionfour:'Anti-dust'}
+  {id:1 ,name:"ALEXA" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Alexa.jpg',descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'2 years motor guarantee',descriptionfour:'Anti-dust'},
+  {id:2 ,name:"BLISS" ,colorone:"Smoke Brown",colortwo:"Plum wine", available:'Available', image:'assets/Bliss.jpg', descriptionone:'Copper winding',descriptiontwo:'GP blade technology',descriptionthree:'Energy saver',descriptionfour:'2 years motor guarantee'},
+  {id:3 ,name:"IMPERIAL" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Imperial.jpg' , descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'2 years motor guarantee',descriptionfour:'Anti-dust'},
+  {id:4 ,name:"SAPPHIRE" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Sapphire.jpg', descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'GP blade technology',descriptionfour:'2 years motor guarantee'},
+  {id:5 ,name:"STAR" ,colorone:"Smoke Brown",colortwo:"Plum wine",available:'Available', image:'assets/Star.jpg' , descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'GP blade technology',descriptionfour:'2 years motor guarantee'},
+  {id:6 ,name:"TURBO+" ,colorone:"Smoke Brown",colortwo:"Ivory",available:'Available', image:'assets/Turbo+.jpg' , descriptionone:'Copper winding',descriptiontwo:'Energy saver',descriptionthree:'2 years motor guarantee',descriptionfour:'Anti-dust'}
 ]
 
 gettotalproducts(){
